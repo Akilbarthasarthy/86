2af8403c7ce39ff0beca4da9322e108045d725e8
Akilbarthasarthy <87700203+Akilbarthasarthy@users.noreply.github.com>
2021-12-09 14:54:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+canvas.add(Block_image_object);
+
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+fabric.Image.fromURL("BirthdayIage.jpg",function(Img)
+{
+Block_image_object.scaletowidth(700);
+Block_image_object.scaletoheight(510);
+Block_image_object.set(
+{
+top:0
+left:0
+
+}
+
+}
+}
+
+function playSound()
+{
+x.play();
+}
